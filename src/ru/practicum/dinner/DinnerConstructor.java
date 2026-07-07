2ef14bd71eff2e51da51316cb2c474e7d4a9db67
FilonovIT <filonovIT@yandex.ru>
2026-07-07 13:31:04 +0400

    Окончательный вариант на проверку

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -57,8 +57,8 @@ public class DinnerConstructor {
     private String getRandomDish(ArrayList<String> availableDishes) {
         int numberOfDishesForType = availableDishes.size(); //получаем общее количество доступных блюд этого типа
         int dishIndex = random.nextInt(numberOfDishesForType); //генерируем случайное число от 0 до (кол-во блюд - 1), чтобы выбрать случайное блюдо
-        String selectedDish = availableDishes.get(dishIndex);//выберем произвольное блюдо по индексу
+        //выберем произвольное блюдо по индексу
 
-        return selectedDish;
+        return availableDishes.get(dishIndex);
     }
 }
